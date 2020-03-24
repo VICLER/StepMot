c656c78ba792841591f24ddd0d9abee7f874ed67
Victor Glekler <victor.glekler@gmail.com>
2020-03-24 03:12:19 +0100

    Update StepMot_example.ino

--- examples/StepMot_example/StepMot_example.ino
+++ examples/StepMot_example/StepMot_example.ino
@@ -4,7 +4,7 @@
  * read about another methods in StepMot.h
  */
 
-#define STEPS_PER_REVOLUTION 2037.88642
+#define STEPS_PER_REVOLUTION 2037.88642 // 28BYJ-48 geared stepper motor
 #define MICROSTEPS  4
 #define STEP_PIN  16
 #define DIR_PIN   10
@@ -15,8 +15,8 @@ StepMot motor(STEPS_PER_REVOLUTION, MICROSTEPS, STEP_PIN, DIR_PIN, EN_PIN);
 void setup() {
   Serial.begin(9600);
   motor.setMode(ABSOLUTE);      // set to ABSOLUTE or RELATIVE modes (default RELATIVE)
-  motor.autoPower(ON);          // when ON motor will be enabled only while moving (default OFF)
-  motor.invertDir(ON);          // software direction inverting 
+  //motor.autoPower(ON);          // when ON motor will be enabled only while moving (default OFF)
+  //motor.invertDir(ON);          // software direction inverting 
   motor.setRPM(10);             // speed in revolutions per minute
   motor.enable();               // power on the motor
 }
