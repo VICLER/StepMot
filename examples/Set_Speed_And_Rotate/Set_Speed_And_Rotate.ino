dd79807c6a18c15ec7dfa57a9d1f6b65893a74cc
Victor Glekler <victor.glekler@gmail.com>
2020-03-25 17:12:06 +0100

    Update Set_Speed_And_Rotate.ino

--- examples/Set_Speed_And_Rotate/Set_Speed_And_Rotate.ino
+++ examples/Set_Speed_And_Rotate/Set_Speed_And_Rotate.ino
@@ -5,18 +5,15 @@
  */
 
 #define STEPS_PER_REVOLUTION 2037.88642 // 28BYJ-48 geared stepper motor
-#define MICROSTEPS  4
+#define MICROSTEPS  2
 #define STEP_PIN  16
 #define DIR_PIN   10
 #define EN_PIN    14
 
-StepMot motor(STEPS_PER_REVOLUTION, MICROSTEPS, STEP_PIN, DIR_PIN, EN_PIN);
+StepMot motor(STEPS_PER_REVOLUTION * MICROSTEPS, STEP_PIN, DIR_PIN, EN_PIN);
 
 void setup() {
   Serial.begin(9600);
-  //motor.setMode(ABSOLUTE);      // set to ABSOLUTE or RELATIVE modes (default RELATIVE)
-  //motor.autoPower(ON);          // when ON motor will be enabled only while moving (default OFF)
-  //motor.invertDir(ON);          // software direction inverting 
   motor.setRPM(10);             // speed in revolutions per minute
   motor.enable();               // power on the motor
 }
@@ -30,5 +27,5 @@ void loop() {
     Serial.println(rpm);
   }
 
-  motor.move();
+  motor.update();
 }
