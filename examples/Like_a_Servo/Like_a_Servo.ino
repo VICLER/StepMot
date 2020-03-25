38e175cdfea40aefcc29c4e5dc713b877542af3a
Victor Glekler <victor.glekler@gmail.com>
2020-03-25 17:11:12 +0100

    Update Like_a_Servo.ino

--- examples/Like_a_Servo/Like_a_Servo.ino
+++ examples/Like_a_Servo/Like_a_Servo.ino
@@ -5,12 +5,12 @@
  */
 
 #define STEPS_PER_REVOLUTION 2037.88642 // 28BYJ-48 geared stepper motor
-#define MICROSTEPS  4
+#define MICROSTEPS  1                   // 1 for full step, 2 for halfstep ...
 #define STEP_PIN  16
 #define DIR_PIN   10
 #define EN_PIN    14
 
-StepMot motor(STEPS_PER_REVOLUTION, MICROSTEPS, STEP_PIN, DIR_PIN, EN_PIN);
+StepMot motor(STEPS_PER_REVOLUTION * MICROSTEPS, STEP_PIN, DIR_PIN, EN_PIN);
 
 void setup() {
   Serial.begin(9600);
